6e98bdb9384c9533a5222f3c8ff330692618929e
Samuel120912 <samuelsihombing11@gmail.com>
2025-06-05 11:47:30 +0700

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+// service-worker.js
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("pemeriksaan-cache-v1").then(cache => {
+      return cache.addAll([
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
